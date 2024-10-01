89009810e945c4b01c03edb46c5bae2e4bed3a2f
AshishChauhan7386 <chauhanashish7386@gmail.com>
2024-10-01 11:06:29 +0530

    Add View Bill Logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewCartComponent } from './component/view-cart/view-cart.component';
+import { UIComponentComponent } from './component/ui-component/ui-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"View-Cart",
+    component:ViewCartComponent
+  },
+  {
+    path:"",
+    component:UIComponentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/component/view-cart/view-cart.component.ts
+++ src/app/component/view-cart/view-cart.component.ts
@@ -4,17 +4,33 @@ import { CartState } from '../../../store/states/cart.state';
 import { cartproduct } from '../../../Models/cart.model';
 import { Observable } from 'rxjs';
 
-
 @Component({
   selector: 'app-view-cart',
   templateUrl: './view-cart.component.html',
-  styleUrl: './view-cart.component.css'
+  styleUrl: './view-cart.component.css',
 })
 export class ViewCartComponent {
-  cartProductData$:Observable<cartproduct[]>;
-constructor(private store:Store){
-  this.cartProductData$=this.store.select(CartState.getProductCart)
- 
+  cartProductData$: Observable<cartproduct[]>;
+  totalpriceempty: boolean = false;
+  totalPrice: any;
+  constructor(private store: Store) {
+    this.cartProductData$ = this.store.select(CartState.getProductCart);
 
-}
+    this.getPrice();
+  }
+
+  getPrice() {
+    this.cartProductData$.subscribe((data) => {
+      if (!data || data.length === 0) {
+        this.totalPrice = null;
+        this.totalpriceempty = false;
+        return;
+      }
+
+      this.totalPrice = data.reduce((total, item) => {
+        return total + item.price * item.quantity;
+      }, 0);
+      this.totalpriceempty = true;
+    });
+  }
 }
